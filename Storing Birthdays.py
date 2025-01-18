7b14c0b2901b4da1e1708835e3fd6e12aa56270a
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-01-18 21:56:30 +0530

    Save

---
+++ Storing Birthdays.py
@@ -0,0 +1,13 @@
+#storing birthdays of Raj's 5 friends
+Krish = "2/01/11"
+Harsh = "4/08/10"
+Cheenu = "6/12/10"
+Sarthak = "25/09/10"
+Ayush = "19/03/11"
+
+print("Krish's birthday is on ",Krish)
+print("\n Harsh's birthday is on", Harsh)
+print("\n Cheenu's birthday is on", Cheenu)
+print("\n Sarthak's birthday is on", Sarthak)
+print("\n Ayush's birthday is on", Ayush) 
+
