ba15f31ee1791ebb12a06103b5dac1f5c6bcc1a0
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-01-19 17:10:53 +0530

    message from Raj to his friend

---
+++ message.py
@@ -0,0 +1,5 @@
+# Raj's congratulation message to his friend in upper case
+text = "congratulation for winning the gold medal in the state games"
+upper_case_text = str.upper(text)
+text = upper_case_text
+print (text) 
