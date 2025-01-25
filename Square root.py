d14cb0f2498e96d9bcc31d3a7a3edcbfa3deb380
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-01-25 19:08:05 +0530

    Square root

---
+++ Square root.py
@@ -0,0 +1,5 @@
+#calculate square root of a number
+import math
+number = float(input("Enter your number: "))
+Square_root = math.sqrt(number)
+print(f"The square root of {number} is {Square_root}")
