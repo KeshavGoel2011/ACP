a60d04fbcbf4e1c3329337356b553d9a0eb51d21
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-01-26 19:01:32 +0530

    Summer Times

---
+++ Summer Times.py
@@ -0,0 +1,8 @@
+#Summer Times
+temp = int(input("Enter the today's temperature: "))
+
+if temp > 25 :
+    print("You can wear light summer clothes")
+
+else:
+    print("You need to wear heavy winter clothes")    
