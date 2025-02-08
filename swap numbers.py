df25fe5567e3afd1c8684802d22030d2a3c0ecab
KeshavGoel2011 <eagleplays2011@gmail.com>
2025-02-08 19:03:14 +0530

    Lesson 8 ACP

---
+++ swap numbers.py
@@ -0,0 +1,10 @@
+#swapping the numbers
+a = int(input("Enter number [a]: "))
+b = int(input("Enter number [b]: "))
+c = int(input("Enter number [c]: "))
+
+temp = a
+b = c
+c = a
+
+print(f"After Swapping [a] = {a}, [b] = {b} and [c] = {c}")
